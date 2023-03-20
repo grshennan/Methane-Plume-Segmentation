ca0c12e7fcb50fa48e1560bd8bf6c13d7e79ba1c
hugogln <hugo.g.perso@gmail.com>
2023-03-20 17:46:58 +0100

    main scripts commit

---
+++ data.py
@@ -0,0 +1,182 @@
+import torch
+import numpy  as np
+import os
+import rasterio as rio
+from torchvision import transforms
+
+
+class PlumeSegmentationDataset():
+    """SmokePlumeSegmentation dataset class."""
+
+    def __init__(self, datadir=None, segdir=None, band=[1,2,3,4,5,6,7,8,9,10,11,12,13], transform=None):
+        
+        self.datadir = datadir
+        self.transform = transform
+        self.band = band
+
+        # list of image files, labels (positive or negative), segmentation
+        self.imgfiles = []
+        self.segfiles = []
+
+        
+        idx=0
+        for root, dirs, files in os.walk(datadir):
+            for filename in files:
+              if not filename.endswith('.tif'):
+                print("Not ending in .tif")
+                continue
+              self.imgfiles.append(os.path.join(root, filename))
+              segfilename = filename.replace(".tif", ".csv")
+              self.segfiles.append(os.path.join(segdir, segfilename))
+              idx+=1
+
+
+        # turn lists into arrays
+        self.imgfiles = np.array(self.imgfiles)
+        self.segfiles = np.array(self.segfiles)
+
+
+    def __len__(self):
+        """Returns length of data set."""
+        return len(self.imgfiles)
+
+
+    def __getitem__(self, idx):
+        """Read in image data, preprocess, build segmentation mask, and apply
+        transformations."""
+
+        # read in image data
+        imgfile = rio.open(self.imgfiles[idx], nodata = 0)
+        imgdata = np.array([imgfile.read(i) for i in self.band])
+        # skip band 11 (Sentinel-2 Band 10, Cirrus) as it does not contain
+        # useful information in the case of Level-2A data products
+
+        fptdata = np.loadtxt(self.segfiles[idx], delimiter=",", dtype=float)
+        fptdata = np.array(fptdata)
+        # fptdata.reshape(fptdata,(120,120))
+
+        sample = {'idx': idx,
+                  'band' : self.band,
+                  'img': imgdata,
+                  'fpt': fptdata,
+                  'imgfile': self.imgfiles[idx]}
+
+        # apply transformations
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
+class Crop(object):
+    """Crop 90x90 pixel image (from 120x120). - TEST FUNCTION"""
+
+    def __call__(self, sample):
+        """
+        :param sample: sample to be cropped
+        :return: randomized sample
+        """
+        imgdata = sample['img']
+
+        x, y = 0, 0
+
+        return {'idx': sample['idx'],
+                'band' : sample['band'],
+                'img': imgdata.copy()[:, 0:90, 0:90],
+                'fpt': sample['fpt'].copy()[0:90, 0:90],
+                'imgfile': sample['imgfile']}
+
+class Randomize(object):
+    """Randomize image orientation including rotations by integer multiples of
+       90 deg, (horizontal) mirroring, and (vertical) flipping."""
+
+    def __call__(self, sample):
+        """
+        :param sample: sample to be randomized
+        :return: randomized sample
+        """
+        imgdata = sample['img']
+        fptdata = sample['fpt']
+
+        # mirror horizontally
+        mirror = np.random.randint(0, 2)
+        if mirror:
+            imgdata = np.flip(imgdata, 2)
+            fptdata = np.flip(fptdata, 1)
+        # flip vertically
+        flip = np.random.randint(0, 2)
+        if flip:
+            imgdata = np.flip(imgdata, 1)
+            fptdata = np.flip(fptdata, 0)
+        # rotate by [0,1,2,3]*90 deg
+        rot = np.random.randint(0, 4)
+        imgdata = np.rot90(imgdata, rot, axes=(1,2))
+        fptdata = np.rot90(fptdata, rot, axes=(0,1))
+
+        return {'idx': sample['idx'],
+                'band' : sample['band'],
+                'img': imgdata.copy(),
+                'fpt': fptdata.copy(),
+                'imgfile': sample['imgfile']}
+
+class Normalize(object):
+    """Normalize pixel values to zero mean and range [-1, +1] measured in
+    standard deviations."""
+    def __init__(self):
+        """
+        :param size: edge length of quadratic output size
+        """
+        self.channel_means = np.array([1909.3802, 1900.5879, 2261.5823, 3164.3564, 3298.6106, 3527.9346, 3791.7458, 3604.5210, 3946.0535, 1223.0176, 27.1881, 4699.9775, 3989.9626])
+        self.channel_stds = np.array([ 498.8658,  507.0728,  573.1718,  965.0130, 1014.2232, 1069.5269, 1133.6522, 1073.3431, 1146.3250,  520.9219,   28.9335, 1360.9994, 1169.5753])
+    
+    def __call__(self, sample):
+        """
+        :param sample: sample to be normalized
+        :return: normalized sample
+        """
+
+        sample['img'] = (sample['img']-self.channel_means[np.array(sample['band'])-1].reshape(
+            sample['img'].shape[0], 1, 1))/self.channel_stds[np.array(sample['band'])-1].reshape(
+            sample['img'].shape[0], 1, 1)
+
+        return sample
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+    def __call__(self, sample):
+        """
+        :param sample: sample to be converted to Tensor
+        :return: converted Tensor sample
+        """
+
+        out = {'idx': sample['idx'],
+               'band' : sample['band'],
+               'img': torch.from_numpy(sample['img'].copy()),
+               'fpt': torch.from_numpy(sample['fpt'].copy()),
+               'imgfile': sample['imgfile']}
+
+        return out
+
+def create_dataset(*args, apply_transforms=True, **kwargs):
+    """Create a dataset; uses same input parameters as PowerPlantDataset.
+    :param apply_transforms: if `True`, apply available transformations
+    :return: data set"""
+    if apply_transforms:
+        data_transforms = transforms.Compose([
+            Normalize(),
+            Crop(),
+            Randomize(),
+            ToTensor()
+           ])
+    else:
+        data_transforms = transforms.Compose([
+            Normalize(),
+            Crop(),
+            ToTensor()
+           ])
+
+    data = PlumeSegmentationDataset(*args, **kwargs,
+                                         transform=data_transforms)
+    
+
+    return data
---
+++ train.py
@@ -0,0 +1,250 @@
+import os
+import numpy as np
+import torch
+from torch import nn, optim
+from tqdm.autonotebook import tqdm
+from sklearn.metrics import accuracy_score
+from torch.utils.data import DataLoader, random_split, RandomSampler
+from torch.utils.tensorboard import SummaryWriter
+import argparse
+from torchmetrics import JaccardIndex, Accuracy
+import time
+import random
+
+from model_unet import *
+from data import create_dataset
+
+
+t = time.localtime()
+crt_time = time.strftime("%H:%M:%S", t)
+idd = random.randint(0, 100)
+print(f"{crt_time}, {idd}")
+
+#Device definition
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+def set_all_seeds(seed):
+  random.seed(seed)
+  np.random.seed(seed)
+  torch.manual_seed(seed)
+  torch.cuda.manual_seed(seed)
+  torch.backends.cudnn.deterministic = True
+
+
+
+def train_model(model, epochs, opt, loss, batch_size):
+    """Wrapper function for model training.
+    :param model: model instance
+    :param epochs: (int) number of epochs to be trained
+    :param opt: optimizer instance
+    :param loss: loss function instance
+    :param batch_size: (int) batch size"""
+    
+    # create dataset
+    PATH_D_TRAIN=os.getcwd() + "/DataTrain/input_tiles/"
+    PATH_S_TRAIN=os.getcwd()+"/DataTrain/output_matrix/"
+    PATH_D_TEST=os.getcwd()+"/DataTest/input_tiles/"
+    PATH_S_TEST=os.getcwd()+"/DataTest/output_matrix/"
+
+    
+    data_train = create_dataset(
+        datadir=PATH_D_TRAIN,
+        segdir=PATH_S_TRAIN,
+        band=bands,
+	apply_transforms=True)
+    
+    data_val = create_dataset(
+        datadir=PATH_D_TEST,
+        segdir=PATH_S_TEST,
+        band=bands,
+        apply_transforms=False)
+    
+    train_sampler = RandomSampler(data_train, replacement=True,
+                                  num_samples=int(2*len(data_train)/3))
+    
+    val_sampler = RandomSampler(data_val, replacement=True,
+                                num_samples=len(data_val))
+
+    # initialize data loaders
+    train_dl = DataLoader(data_train, batch_size=batch_size,
+                          pin_memory=True, sampler=train_sampler)
+    val_dl = DataLoader(data_val, batch_size=batch_size,
+                         pin_memory=True, sampler=val_sampler)
+
+    
+
+
+
+    # start training process
+    for epoch in range(epochs):
+
+        model.train()
+
+        
+        train_loss_total = 0
+        train_ious = []
+        train_acc_total = 0
+        train_area = []
+
+       
+        for i, batch in enumerate(train_dl):
+            x = batch['img'].float().to(device)
+            y = batch['fpt'].float().to(device)
+
+            output = model(x)
+
+            
+
+           
+            #derive segmentation map from prediction
+            output_bin = torch.round(torch.sigmoid(output))
+
+            # derive IoU values
+            jaccard = JaccardIndex(task = 'binary').to(device)
+            z = jaccard(output_bin, y.unsqueeze(dim=1)).cpu()
+            train_ious.append(z.detach().numpy())
+
+            # derive image-wise accuracy for this batch
+            acc = Accuracy(task = 'binary').to(device)
+            a = acc(output_bin, y[:,None,:,:])
+            
+            train_acc_total += a
+
+            # derive loss
+            loss_epoch = loss(output, y.unsqueeze(dim=1))
+            train_loss_total += loss_epoch
+         
+            # derive smoke areas
+            area_pred = torch.sum(output_bin, dim = (1, 2, 3))
+            area_true = torch.sum(y.unsqueeze(dim=1), dim=(1,2,3))
+
+            #Derive area accuracy
+            area_dif = torch.sum(torch.square(torch.sub(area_pred, area_true))).cpu()
+            train_area.append(area_dif.detach().numpy())
+
+            # learning
+            opt.zero_grad()
+            loss_epoch.backward()
+            opt.step()
+
+            # logging
+            writer.add_scalar("Train/Loss", train_loss_total/(i+1), epoch)
+            writer.add_scalar("Train/Iou", np.average(train_ious), epoch)
+            writer.add_scalar("Train/Acc", train_acc_total/(i+1), epoch)
+            writer.add_scalar('Train/Arearatio mean', np.average(train_area), epoch)
+            writer.add_scalar('Train/Arearatio std', np.std(train_area), epoch)
+            writer.add_scalar('Train/learning_rate', opt.param_groups[0]['lr'], epoch)
+
+            torch.cuda.empty_cache()
+
+        # evaluation 
+        with torch.no_grad():
+            val_loss_total = 0
+            val_ious = []
+            val_acc_total = 0
+            val_area = []
+          
+            for j, batch in enumerate(val_dl):
+                x = batch['img'].float().to(device)
+                y = batch['fpt'].float().to(device)
+
+                output = model(x)
+
+                # derive loss
+                loss_epoch = loss(output, y.unsqueeze(dim=1))
+                val_loss_total += loss_epoch
+                
+                #derive segmentation map from prediction
+                output_bin = torch.round(torch.sigmoid(output))
+
+                # derive IoU values
+                jaccard = JaccardIndex(task = 'binary').to(device)
+                z = jaccard(output_bin, y.unsqueeze(dim=1)).cpu()
+                val_ious.append(z.detach().numpy())
+
+                # derive image-wise accuracy for this batch
+                acc = Accuracy(task = 'binary').to(device)
+                a = acc(output_bin, y[:,None,:,:])
+
+                val_acc_total += a
+
+                # derive smoke areas
+                area_pred = torch.sum(output_bin, dim = (1, 2, 3))
+                area_true = torch.sum(y.unsqueeze(dim=1), dim=(1,2,3))
+
+                #Derive area accuracy
+                area_dif = torch.sum(torch.square(torch.sub(area_pred, area_true))).cpu()
+                val_area.append(area_dif.detach().numpy())
+              
+             
+
+
+                # logging
+                writer.add_scalar("Test/Loss", val_loss_total/(j+1), epoch)
+                writer.add_scalar("Test/Iou", np.average(val_ious), epoch)
+                writer.add_scalar("Test/Acc", val_acc_total/(j+1), epoch)
+                writer.add_scalar('Test/Arearatio mean',
+                               np.average(val_area), epoch)
+                writer.add_scalar('Test/Arearatio std',
+                               np.std(val_area), epoch)
+
+            print(("Epoch {:d}: train loss={:.3f}, val loss={:.3f}, " "train iou={:.3f}," "train acc={:.3f},").format(epoch+1, train_loss_total/(i+1), val_loss_total/(j+1), np.average(train_ious),train_acc_total/(i+1)))
+            
+            writer.flush()
+
+            
+            if epoch%50 == 0:
+                PATH_MOD=os.getcwd()+"/Methane_training_data/mod/"
+                torch.save(model.state_dict(), PATH_MOD+f"ep{epoch}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}.model")
+
+
+    return model
+
+
+
+
+
+
+
+
+################## MAIN ##################
+
+#Seed
+set_all_seeds(21)
+
+
+#Path du Working Directory modifier "../../../tmpdir/{USERNAME}"
+PATH_WD = "../../../tmpdir/guiglion"
+os.chdir(PATH_WD)
+
+#Model
+bands = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+model = UNet(n_channels=13, n_classes=1)
+model.to(device)
+
+#Trainning parameters
+ep = 200
+lr = 0.01
+bs = 30
+
+# setup tensorboard writer
+PATH_RUNS =os.getcwd()+'/Methane_training_data/runs/'
+writer = SummaryWriter(PATH_RUNS+ f"ep{ep}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}/")
+
+# initialize loss function
+loss = nn.BCEWithLogitsLoss()
+
+# initialize optimizer
+opt = optim.Adam(model.parameters(),lr=lr)
+
+### run training
+trained_model = train_model(model, ep, opt, loss, bs)
+
+
+PATH_MOD=os.getcwd()+"/Methane_training_data/mod/"
+torch.save(trained_model.state_dict(),
+PATH_MOD+f"ep{ep}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}.model")
+
+
+writer.close()
