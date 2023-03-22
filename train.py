fefd240f2d911554dc5a774df75b90c2de799c4e
hugogln <hugo.g.perso@gmail.com>
2023-03-22 11:23:08 +0100

    resolved paths issues

--- train.py
+++ train.py
@@ -42,10 +42,10 @@ def train_model(model, epochs, opt, loss, batch_size):
     :param batch_size: (int) batch size"""
     
     # create dataset
-    PATH_D_TRAIN=os.getcwd() + "/DataTrain/input_tiles/"
-    PATH_S_TRAIN=os.getcwd()+"/DataTrain/output_matrix/"
-    PATH_D_TEST=os.getcwd()+"/DataTest/input_tiles/"
-    PATH_S_TEST=os.getcwd()+"/DataTest/output_matrix/"
+    PATH_D_TRAIN=os.getcwd() + "/data/DataTrain/input_tiles/"
+    PATH_S_TRAIN=os.getcwd()+"/data/DataTrain/output_matrix/"
+    PATH_D_TEST=os.getcwd()+"/data/DataTest/input_tiles/"
+    PATH_S_TEST=os.getcwd()+"/data/DataTest/output_matrix/"
 
     
     data_train = create_dataset(
@@ -195,7 +195,7 @@ def train_model(model, epochs, opt, loss, batch_size):
 
             
             if epoch%50 == 0:
-                PATH_MOD=os.getcwd()+"/Methane_training_data/mod/"
+                PATH_MOD=os.getcwd()+"/mod/"
                 torch.save(model.state_dict(), PATH_MOD+f"ep{epoch}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}.model")
 
 
@@ -209,42 +209,57 @@ def train_model(model, epochs, opt, loss, batch_size):
 
 
 ################## MAIN ##################
+if __name__ == '__main__':
 
-#Seed
-set_all_seeds(21)
-
-
-#Path du Working Directory modifier "../../../tmpdir/{USERNAME}"
-PATH_WD = "../../../tmpdir/guiglion"
-os.chdir(PATH_WD)
-
-#Model
-bands = [1,2,3,4,5,6,7,8,9,10,11,12,13]
-model = UNet(n_channels=13, n_classes=1)
-model.to(device)
-
-#Trainning parameters
-ep = 200
-lr = 0.01
-bs = 30
-
-# setup tensorboard writer
-PATH_RUNS =os.getcwd()+'/Methane_training_data/runs/'
-writer = SummaryWriter(PATH_RUNS+ f"ep{ep}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}/")
-
-# initialize loss function
-loss = nn.BCEWithLogitsLoss()
-
-# initialize optimizer
-opt = optim.Adam(model.parameters(),lr=lr)
-
-### run training
-trained_model = train_model(model, ep, opt, loss, bs)
+    # setup argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ep', type=int, default=300,
+                    help='Number of epochs')
+    parser.add_argument('-bs', type=int, nargs='?',
+                    default=60, help='Batch size')
+    parser.add_argument('-lr', type=float,
+                    nargs='?', default=0.01, help='Learning rate')
+    args = parser.parse_args()
 
 
-PATH_MOD=os.getcwd()+"/Methane_training_data/mod/"
-torch.save(trained_model.state_dict(),
-PATH_MOD+f"ep{ep}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}.model")
+    #Seed
+    set_all_seeds(21)
+    
+    
+    #Path du Working Directory modifier "../../../tmpdir/{USERNAME}"
+    #PATH_WD = "../../../tmpdir/guiglion"
+    #os.chdir(PATH_WD)
+    
+    #Model
+    bands = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    model = UNet(n_channels=13, n_classes=1)
+    model.to(device)
+    
+    #Trainning parameters
+    ep = args.ep 
+    lr = args.lr
+    bs = args.bs
 
+    # setup tensorboard writer
+    PATH_RUNS =os.getcwd()+'/runs/'
+    writer = SummaryWriter(PATH_RUNS+ f"ep{ep}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}/")
+    
+    # initialize loss function
+    loss = nn.BCEWithLogitsLoss()
+    
+    # initialize optimizer
+    opt = optim.Adam(model.parameters(),lr=lr)
+   
+    PATH_MOD=os.getcwd()+"/mod/"
+    if not os.path.exists(PATH_MOD):
+        os.mkdir(PATH_MOD)
 
-writer.close()
+    # run training
+    trained_model = train_model(model, ep, opt, loss, bs)
+    
+    # save final trained model
+    torch.save(trained_model.state_dict(),
+    PATH_MOD+f"ep{ep}_lr{lr}_bs{bs}_time{crt_time}_idd{idd}.model")    
+    
+    
+    writer.close()
